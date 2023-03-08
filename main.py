0dc27e8b148c0592261b28c149b791bc522fdf13
Ken <kenoh17@uw.edu>
2023-03-08 12:15:42 -0800

    started on setting up plotly and dash

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
+from dash import Dash, html, dcc, Input, Output
 import pandas as pd
 import data_processing as dp
+pd.options.plotting.backend = "plotly"
+
+# Datasets
+EDU_DF = dp.clean_edu_data()
+INCOME_DF = dp.clean_income_data()
+
+# Helpful Global Variables
+EDU_RACES = EDU_DF["Race"].sort_values().unique()
+
+app = Dash()
+
+race_dropdown = dcc.Dropdown(options=EDU_RACES, value=EDU_RACES[0])
+
+
+def attainment_over_time(df: pd.DataFrame):
+    pass
 
 
 def main():
-    edu_df = dp.clean_edu_data()
-    income_df = dp.clean_income_data()
+    print(EDU_RACES)
+    # attainment_over_time(EDU_DF)
 
 
 if __name__ == '__main__':
