4aa00812f26962cbe7f7b355256293a25120c032
Ken <kenoh17@uw.edu>
2023-03-12 22:34:04 -0700

    analyzed question 1

--- pages/question1.py
+++ pages/question1.py
@@ -44,13 +44,35 @@ layout = html.Div(children=[
 
         The thing that stood out the most after looking at the graphs is the
         most prominent degree among all races is less than high school.
-        One thing to note is that while white has a really high number of
-        people who have less than high school, the proportion of those
-        people vs those who have a high school diploma are similar to the
-        other races. The most concerning thing we noticed is that
+        If you look at each race individually, the line that shows up at the
+        top is "less than high school".
+        One thing to note is that even though the number of people who attained
+        a certain degree is higher in a certain race, we will look at the
+        proportion instead of count in order to reduce bias, since White is the
+        majority. The most concerning thing we noticed is that
         Hispanic/Latino have the biggest disparity between "less than
-        high school" and any degree higher than that, averaging a gap of
-        around 700k people. We decided to take a closer look at this
+        high school" and any degree higher than that, a ratio of 10:3
+        to the second highest degree. For reference, the ratio for
+        White is around 23:21, Asian is 33:28, and Black is 17:11.
+        These are all significantly smaller than Hispanic/Latino.
+
+        The question we are trying to answer with this graph is:
+        **How much disparity is there between the most attained degree and
+        second most? What could be causing this disparity?**
+
+        We know that for all races, "less than high school" is the most
+        attained degree, however, what is causing the disparity to be
+        so large among Hispanic/Latino? My first thought went to immigration.
+        Because most Hispanics move to America not knowing much English, they
+        probably have a hard time getting work, which leads to lower income,
+        and living in poorer neighborhoods. Poorer neighborhoods means worse
+        education, and the environment a student is in is a huge factor
+        in how well they do in school. If they're poorly influenced by 
+        peers and teachers don't teach with care and efficiency, they may end
+        up dropping out or just never graduating. Along with this, there
+        may be pressure to work from a young age in order to support
+        their family, which takes away necessary time for studying and
+        doing well in school.
         ''', className='markdown')
     ], className='question-one')
 ], className='whole-page')
