e7170ca5337419262b8e53febbb917cebcbb9de4
soojah <115122666+soojah@users.noreply.github.com>
2023-03-13 16:50:59 -0700

    merged health and working on page for q

---
+++ pages/question6.py
@@ -0,0 +1,36 @@
+# Setup
+import dash
+from dash import html, dcc, Input, Output, callback
+import pandas as pd
+import data_processing as dp
+import plotly.express as px
+import numpy as np
+pd.options.plotting.backend = "plotly"
+
+# Datasets
+EDU_DF = dp.clean_edu_data()
+HEALTH_DF = dp.merge_health()
+# Dropdown options
+COUNTIES = np.insert(HEALTH_DF['County'].sort_values().unique(), 0,
+                    'Select All')
+
+# Create dropdowns
+region_dropdown = dcc.Dropdown(options=COUNTIES, value=[],
+                               placeholder='Select a Region',
+                               className='dropdown-two', multi=True)
+
+# Create layout for this page
+layout = html.Div(children=[
+    html.H1(children='Education Attainment Over Time, \
+            Compared to Health', className='graph-header'),
+    html.Div(children=[
+        html.Div([
+            region_dropdown,
+            dcc.Graph(id='graph1')
+        ], className='graphs'),
+        dcc.Markdown('''
+        # What did we find?
+
+        ''', className='markdown')
+    ], className='question-six')
+], className='whole-page')
