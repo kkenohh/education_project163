21ca8d69491a2908b6b0fcef94bbe13b00704e5c
Ken <kenoh17@uw.edu>
2023-03-13 21:06:09 -0700

    conclusion page added

---
+++ pages/zconclusion.py
@@ -0,0 +1,20 @@
+import dash
+from dash import html, dcc
+
+dash.register_page(__name__, name='Final Thoughts')
+
+layout = html.Div(children=[
+    dcc.Markdown('''
+    # Conclusion
+
+    ''', className='markdown home'),
+    dcc.Markdown('''
+    # 
+
+    ''', className='markdown home'),
+    dcc.Markdown('''
+    # Machine Learning Results
+
+    
+    ''', className='markdown home')
+], className='home-div')
